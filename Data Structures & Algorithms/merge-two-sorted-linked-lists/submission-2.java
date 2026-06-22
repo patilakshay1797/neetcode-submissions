3fbc788f92a45625ff794b317cf17121e72464fd
patilakshay1797 <97301771+patilakshay1797@users.noreply.github.com>
2026-06-22 22:29:59 +0530

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        ListNode head;
+        if(list1.val > list2.val) {
+            head = list2;
+            list2 = list2.next;
+        } else {
+            head = list1;
+            list1 = list1.next;
+        }
+
+        ListNode mergedNode = head;
+
+        while(list1 != null || list2 != null) {
+            if(list1 == null) {
+                mergedNode.next = list2;
+                if(list2 != null)
+                    list2 = list2.next;
+                else list2 = null;
+                mergedNode = mergedNode.next;
+            }
+            if(list2 == null) {
+                mergedNode.next = list1;
+                if(list1 != null)
+                    list1 = list1.next;
+                else list1 = null;
+                mergedNode = mergedNode.next;
+            }
+            if(list1 != null && list2 != null) {
+                if(list1.val > list2.val) {
+                    mergedNode.next = list2;
+                    list2 = list2.next;
+                    mergedNode = mergedNode.next;
+                } else {
+                    mergedNode.next = list1;
+                    list1 = list1.next;
+                    mergedNode = mergedNode.next;
+                }
+            }
+            // System.out.println("end of while -- " + list1.val + " -- " + list2.val);
+            // System.out.println("merged Node -- " + mergedNode.val);
+        }
+        return head;
+    }
+}
