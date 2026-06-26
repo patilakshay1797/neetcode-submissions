1d647cfe3d5b973685f8f0d3d77b430f0286a19d
patilakshay1797 <97301771+patilakshay1797@users.noreply.github.com>
2026-06-26 22:08:08 +0530

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Time complexity ==> O(m * log(n))
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean isTargetPresent = false;
+        for(int i = 0; i < matrix.length; i++) {
+            System.out.println(matrix[i][0]);
+            if(Arrays.binarySearch(matrix[i], target) > -1) {
+                isTargetPresent = true;
+                break;
+            }
+        }
+        return isTargetPresent;
+    }
+}
